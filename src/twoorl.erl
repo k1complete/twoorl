244831e6a7f468b47b3b4a8cf3eea1582403b603
Davide <nesrait@gmail.com>
2008-12-14 09:55:39 +0000

    Changed the unsupervised boot to use the same functions as the "OTP application" boot and removed the deprecated functions.

--- src/twoorl.erl
+++ src/twoorl.erl
@@ -68,7 +68,7 @@ mysql_connect(PoolSize, Hostname, User, Password, Database) ->
       fun(_PoolNumber) ->
 	      mysql:connect(erlydb_mysql, Hostname, undefined, User, Password,
 			    Database, true)
-      end, lists:seq(1, PoolSize - 1)).
+      end, lists:seq(1, PoolSize)).
 
 compile() ->
     compile([]).
@@ -93,71 +93,12 @@ compile_dir(appconfig) ->
 compile_dir(Dir) ->
     Dir.
 
-%% --- The rest of these functions will be deprecated --- %%
-
 start() ->
-    application:start(inets),
-    init_mnesia(),
-    TablesInfo = [{session, [{attributes, record_info(fields, session)}]}],
-    create_mnesia_tables(TablesInfo),
-    init_mysql(),
-    compile().
-
-init_mnesia() ->
-    ?L("creating schema"),
-    case mnesia:create_schema([node()]) of
-        {error, {_,{already_exists, _}}} ->
-            ?L("schema already exists");
-	{error, Err} ->
-            ?L("schema creation error, aborting"),
-            exit(Err);
-        ok ->
-            ok
-    end,
-    case mnesia:start() of
-        {error, Err1}  ->
-            ?L("error starting mnesia"),
-            exit(Err1);
-        ok -> ok
-    end.
-
-create_mnesia_tables(TablesInfo) ->
-    ExistingTables = mnesia:system_info(tables) -- [schema],
-    TablesToCreate = [TableInfo || TableInfo = {Name, _Atts} <- TablesInfo,
-				   not lists:member(Name, ExistingTables)],
-    lists:foreach(
-      fun({TableName, Atts}) ->
-	      create_table(TableName, Atts)
-      end, TablesToCreate),
-    
-    ?L({"waiting for tables", ExistingTables}),
-    case mnesia:wait_for_tables(ExistingTables, 20000) of
-	{timeout, RemainingTabs} ->
-	    exit({mnesia_timeout, RemainingTabs});
-        ok ->
-            ok
-    end.
-
-create_table(Table, Def) ->
-    ?L("creating table: " ++ atom_to_list(Table)),
-    case mnesia:create_table(Table, Def) of
-        {atomic, ok} -> ok;
-        {aborted, Err2} ->
-            ?L("create table error"),
-            exit(Err2)
-    end.
-
-init_mysql() ->
-    erlydb:start(mysql,
-		 [{hostname, ?DB_HOSTNAME},
-		  {username, ?DB_USERNAME}, {password, ?DB_PASSWORD},
-		  {database, ?DB_DATABASE},
-		  {logfun, fun log/4}]),
-    lists:foreach(
-      fun(_) ->
-	      mysql:connect(erlydb_mysql, ?DB_HOSTNAME, undefined,
-			    ?DB_USERNAME, ?DB_PASSWORD, ?DB_DATABASE, true)
-      end, lists:seq(1, ?DB_POOL_SIZE)).
-
-log(Module, Line, Level, FormatFun) ->
-    twoorl_util:log(Module, Line, Level, FormatFun).
+    process_flag(trap_exit, true),
+    Inets = (catch application:start(inets)),
+    Crypto = (catch application:start(crypto)),
+    Mnesia = (catch application:start(mnesia)),
+    start_phase(mysql, normal, [{?DB_HOSTNAME, ?DB_USERNAME, ?DB_PASSWORD, ?DB_DATABASE, ?DB_POOL_SIZE}]),
+    start_phase(mnesia, normal, [session]),
+    start_phase(compile, normal, []),
+    [{inets, Inets}, {crypto, Crypto}, {mnesia, Mnesia}].
